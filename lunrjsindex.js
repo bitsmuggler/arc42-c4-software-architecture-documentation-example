d4dbc72321d75119c65960c2cfc7b7200f66a2bd
bitsmuggler <bitsmuggler@users.noreply.github.com>
2023-02-20 19:50:09 +0000

    deploy: 0b7b5c7ad2a600381517bf4be43845e9b553bcf4

---
+++ lunrjsindex.js
@@ -0,0 +1,115 @@
+var documents = [
+
+{
+    "id": 0,
+    "uri": "arc42/12_glossary.html",
+    "menu": "arc42",
+    "title": "Glossary",
+    "text": " Table of Contents Glossary Glossary Term Definition &lt;Term-1&gt; &lt;definition-1&gt; &lt;Term-2&gt; &lt;definition-2&gt; "
+},
+
+{
+    "id": 1,
+    "uri": "arc42/10_quality_requirements.html",
+    "menu": "arc42",
+    "title": "Quality Requirements",
+    "text": " Table of Contents Quality Requirements Quality Tree Quality Scenarios Quality Requirements Quality Tree Quality Scenarios "
+},
+
+{
+    "id": 2,
+    "uri": "arc42/09_architecture_decisions.html",
+    "menu": "arc42",
+    "title": "Architecture Decisions",
+    "text": " Table of Contents Architecture Decisions Architecture Decisions "
+},
+
+{
+    "id": 3,
+    "uri": "arc42/06_runtime_view.html",
+    "menu": "arc42",
+    "title": "Runtime View",
+    "text": " Table of Contents Runtime View Sign-In &lt;Runtime Scenario 2&gt; &#8230;&#8203; &lt;Runtime Scenario n&gt; Runtime View Sign-In Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-SignIn.puml[] Legende Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-SignIn-key.puml[] &lt;insert runtime diagram or textual description of the scenario&gt; &lt;insert description of the notable aspects of the interactions between the building block instances depicted in this diagram.&gt; &lt;Runtime Scenario 2&gt; &#8230;&#8203; &lt;Runtime Scenario n&gt; "
+},
+
+{
+    "id": 4,
+    "uri": "arc42/05_building_block_view.html",
+    "menu": "arc42",
+    "title": "Building Block View",
+    "text": " Table of Contents Building Block View Internet Banking System Level 2 Level 3 Building Block View Internet Banking System Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-Containers.puml[] Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-Containers-key.puml[] Legende Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-Containers-key.puml[] &lt;Overview Diagram&gt; Motivation &lt;text explanation&gt; Contained Building Blocks &lt;Description of contained building block (black boxes)&gt; Important Interfaces &lt;Description of important interfaces&gt; &lt;Name black box 1&gt; &lt;Purpose/Responsibility&gt; &lt;Interface(s)&gt; &lt;(Optional) Quality/Performance Characteristics&gt; &lt;(Optional) Directory/File Location&gt; &lt;(Optional) Fulfilled Requirements&gt; &lt;(optional) Open Issues/Problems/Risks&gt; &lt;Name black box 2&gt; &lt;black box template&gt; &lt;Name black box n&gt; &lt;black box template&gt; &lt;Name interface 1&gt; &#8230;&#8203; &lt;Name interface m&gt; Level 2 White Box API Application Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-Components.puml[] Legende Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-Components-key.puml[] &lt;white box template&gt; White Box &lt;building block 2&gt; &lt;white box template&gt; &#8230;&#8203; White Box &lt;building block m&gt; &lt;white box template&gt; Level 3 White Box &lt;_building block x.1_&gt; &lt;white box template&gt; White Box &lt;_building block x.2_&gt; &lt;white box template&gt; White Box &lt;_building block y.1_&gt; &lt;white box template&gt; "
+},
+
+{
+    "id": 5,
+    "uri": "arc42/11_technical_risks.html",
+    "menu": "arc42",
+    "title": "Risks and Technical Debts",
+    "text": " Table of Contents Risks and Technical Debts Risks and Technical Debts "
+},
+
+{
+    "id": 6,
+    "uri": "arc42/07_deployment_view.html",
+    "menu": "arc42",
+    "title": "Deployment View",
+    "text": " Table of Contents Deployment View Infrastructure Level 1 Infrastructure Level 2 Deployment View Infrastructure Level 1 Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-LiveDeployment.puml[] Legende Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-LiveDeployment-key.puml[] Motivation &lt;explanation in text form&gt; Quality and/or Performance Features &lt;explanation in text form&gt; Mapping of Building Blocks to Infrastructure &lt;description of the mapping&gt; Infrastructure Level 2 &lt;Infrastructure Element 1&gt; &lt;diagram + explanation&gt; &lt;Infrastructure Element 2&gt; &lt;diagram + explanation&gt; &#8230;&#8203; &lt;Infrastructure Element n&gt; &lt;diagram + explanation&gt; "
+},
+
+{
+    "id": 7,
+    "uri": "arc42/02_architecture_constraints.html",
+    "menu": "arc42",
+    "title": "Architecture Constraints",
+    "text": " Table of Contents Architecture Constraints Architecture Constraints "
+},
+
+{
+    "id": 8,
+    "uri": "arc42/01_introduction_and_goals.html",
+    "menu": "arc42",
+    "title": "Introduction and Goals",
+    "text": " Table of Contents Introduction and Goals Requirements Overview Quality Goals Stakeholders Introduction and Goals Requirements Overview Quality Goals Stakeholders Role/Name Contact Expectations &lt;Role-1&gt; &lt;Contact-1&gt; &lt;Expectation-1&gt; &lt;Role-2&gt; &lt;Contact-2&gt; &lt;Expectation-2&gt; "
+},
+
+{
+    "id": 9,
+    "uri": "arc42/08_concepts.html",
+    "menu": "arc42",
+    "title": "Cross-cutting Concepts",
+    "text": " Table of Contents Cross-cutting Concepts &lt;Concept 1&gt; &lt;Concept 2&gt; &lt;Concept n&gt; Cross-cutting Concepts &lt;Concept 1&gt; &lt;explanation&gt; &lt;Concept 2&gt; &lt;explanation&gt; &#8230;&#8203; &lt;Concept n&gt; &lt;explanation&gt; "
+},
+
+{
+    "id": 10,
+    "uri": "arc42/04_solution_strategy.html",
+    "menu": "arc42",
+    "title": "Solution Strategy",
+    "text": " Table of Contents Solution Strategy Solution Strategy "
+},
+
+{
+    "id": 11,
+    "uri": "arc42/03_system_scope_and_context.html",
+    "menu": "arc42",
+    "title": "System Scope and Context",
+    "text": " Table of Contents System Scope and Context Business Context Technical Context System Scope and Context Business Context Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-SystemContext.puml[] Legende Failed to generate image: 400 Bad Request Unresolved directive in &lt;stdin&gt; - include::../diagrams/structurizr-SystemContext-key.puml[] &lt;Diagram or Table&gt; &lt;optionally: Explanation of external domain interfaces&gt; Technical Context &lt;Diagram or Table&gt; &lt;optionally: Explanation of technical interfaces&gt; &lt;Mapping Input/Output to Channels&gt; "
+},
+
+{
+    "id": 12,
+    "uri": "search.html",
+    "menu": "-",
+    "title": "search",
+    "text": " Search Results "
+},
+
+{
+    "id": 13,
+    "uri": "lunrjsindex.html",
+    "menu": "-",
+    "title": "null",
+    "text": " will be replaced by the index "
+},
+
+];
